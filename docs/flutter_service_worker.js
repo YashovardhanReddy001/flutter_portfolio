aca4ff8fc9196c0ad03120097c38811a8933fc4a
YashovardhanReddy001 <yashovardhanreddy82@gmail.com>
2025-09-29 15:44:51 +0530

    Update web build (fix base href)

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "16853c87abe1fff6c52a436f445ae49b
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "2381ce27abd4fb47e52b6bdfdc9f1449",
+"flutter_bootstrap.js": "e300f209749cfe0c50e969f3dadd0edc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
